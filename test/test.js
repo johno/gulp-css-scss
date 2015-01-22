0ccccc9b1b8994812f382ab1015562310bf7c94c
John Otander <johnotander@gmail.com>
2015-01-21 17:09:55 -0700

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+var gulpCssScss = require('..');
+
+describe('gulp-css-scss', function() {
+
+  it('should do something awesome', function() {
+    assert.equal(gulpCssScss(), true);
+  });
+});
