c2fa3b5b429b8929259846b7d9f1ae7a89e94046
John Otander <johnotander@gmail.com>
2015-01-21 17:25:56 -0700

    Implement base functionality, add test

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 'use strict';
 
+var through = require('through2');
+var cssscss = require('css-scss');
+var gutil = require('gulp-util');
+
 module.exports = function gulpCssScss(options) {
   options = options || {};
 
-  return true;
+  return through.obj(function(file, enc, cb) {
+    if (file.isNull()) {
+      cb(null, file);
+      return;
+    }
+
+    if (!file.isBuffer()) {
+      cb();
+    }
+
+    var src = file.contents.toString();
+    var css = cssscss(src);
+
+    file.path = gutil.replaceExtension(file.path, '.scss');
+
+    file.contents = new Buffer(css);
+    cb(null, file);
+  });
 }
--- test/test.js
+++ test/test.js
@@ -1,9 +1,27 @@
 var assert = require('assert');
+var gutil = require('gulp-util');
 var gulpCssScss = require('..');
 
 describe('gulp-css-scss', function() {
 
-  it('should do something awesome', function() {
-    assert.equal(gulpCssScss(), true);
+  it('should convert CSS to Scss', function(done) {
+    var cssScssStream = gulpCssScss();
+
+    var actual = ':root {\n  --blue: blue;\n}\n\n.some-class {\n  color: var(--blue);\n}\n\n';
+    var expected = '\n// Converted Variables\n\n$blue: blue !default;\n\n// Custom Media Query Variables\n\n\n.some-class {\n  color: $blue;\n}\n\n';
+
+    cssScssStream.once('data', function(file) {
+      assert.equal(file.relative, 'default.scss');
+      assert.equal(file.contents.toString(), expected);
+    });
+
+    cssScssStream.on('end', done);
+
+    cssScssStream.write(new gutil.File({
+      path: 'default.css',
+      contents: new Buffer(actual)
+    }));
+
+    cssScssStream.end();
   });
 });
