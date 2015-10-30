09c87183f7d554846e458cfb7ac6b252a36f6802
John Otander <johnotander@gmail.com>
2015-10-30 16:26:30 -0600

    Use ava

--- test.js
+++ test.js
@@ -1,27 +1,24 @@
-var assert = require('assert');
-var gutil = require('gulp-util');
-var gulpCssScss = require('./');
+import test from 'ava'
+import gutil from 'gulp-util'
+import gulpCssScss from './'
 
-describe('gulp-css-scss', function() {
+test('converts CSS to Scss', t => {
+  t.plan(2)
 
-  it('should convert CSS to Scss', function(done) {
-    var cssScssStream = gulpCssScss();
+  var cssScssStream = gulpCssScss();
 
-    var actual = ':root {\n  --blue: blue;\n}\n\n.some-class {\n  color: var(--blue);\n}\n\n';
-    var expected = '\n// Converted Variables\n\n$blue: blue !default;\n\n// Custom Media Query Variables\n\n\n.some-class {\n  color: $blue;\n}\n\n';
+  var actual = ':root {\n  --blue: blue;\n}\n\n.some-class {\n  color: var(--blue);\n}\n\n';
+  var expected = '\n// Converted Variables\n\n$blue: blue !default;\n\n// Custom Media Query Variables\n\n\n.some-class {\n  color: $blue;\n}\n\n';
 
-    cssScssStream.once('data', function(file) {
-      assert.equal(file.relative, 'default.scss');
-      assert.equal(file.contents.toString(), expected);
-    });
-
-    cssScssStream.on('end', done);
-
-    cssScssStream.write(new gutil.File({
-      path: 'default.css',
-      contents: new Buffer(actual)
-    }));
-
-    cssScssStream.end();
+  cssScssStream.once('data', file => {
+    t.same(file.relative, 'default.scss');
+    t.same(file.contents.toString(), expected);
   });
+
+  cssScssStream.write(new gutil.File({
+    path: 'default.css',
+    contents: new Buffer(actual)
+  }));
+
+  cssScssStream.end();
 });
